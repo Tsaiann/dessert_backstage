3a6ca3bd2913bf8bf1f82f2d4ba82ac443ab6e7e
ann <ann110149@gmail.com>
2022-03-23 22:49:57 +0800

    update

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
+import Cookies from 'js-cookie'
 
 const routes = [
   {
@@ -7,35 +8,41 @@ const routes = [
     component: () => import('../views/Login.vue'),
   },
   {
-    path: '/Home',
+    path: '/home',
     name: 'Home',
     component: () => import('../views/Home.vue'),
+    meta:{ requireAuth: true },
     redirect:'dashboard',
     children:[
       {
-        path: '',
+        path: 'dashboard',
         name: 'Dashboard',
-        component: () => import('../components/dashboard.vue')
+        component: () => import('../components/dashboard.vue'),
+        meta:{ requireAuth: true }
       },
       {
         path: 'order',
         name: 'Order',
-        component: () => import('../components/order.vue')
+        component: () => import('../components/order.vue'),
+        meta:{ requireAuth: true },
       },
       {
         path: 'product',
         name: 'Product',
-        component: () => import('../components/product.vue')
+        component: () => import('../components/product.vue'),
+        meta:{ requireAuth: true },
       },
       {
         path: 'user',
         name: 'User',
-        component: () => import('../components/user.vue')
+        component: () => import('../components/user.vue'),
+        meta:{ requireAuth: true },
       },
       {
         path: 'manager',
         name: 'Manager',
-        component: () => import('../components/manager.vue')
+        component: () => import('../components/manager.vue'),
+        meta:{ requireAuth: true },
       }
     ]
   }
@@ -46,4 +53,21 @@ const router = createRouter({
   routes
 })
 
+router.beforeEach(async(to, from, next) => {
+  if(to.meta.requireAuth){
+    const userInfo = Cookies.get('login')
+    if(userInfo){
+      const token = JSON.parse(userInfo).token
+      if(token.length > 0){
+        next()
+      }else{
+        next({ name: 'Login'})
+      }
+    }else{
+        next({ name: 'Login'})
+    }
+  }else{
+    next()
+  }
+})
 export default router
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,29 +1,54 @@
 <template>
   <div class="login row vertical" data-width="20%">
-    <el-input :model="loginForm.username" type="text" placeholder="請輸入帳號" />
-    <el-input :model="loginForm.password" type="password" placeholder="請輸入密碼" data-space-bottom="1rem"/>
+    <el-input v-model="loginForm.username" type="text" placeholder="請輸入帳號" />
+    <el-input v-model="loginForm.password" type="password" placeholder="請輸入密碼" data-space-bottom="1rem"/>
     <div class="row horizontal center">
-      <el-button type="primary">登入</el-button>
-      <el-button>清除</el-button>
+      <el-button type="primary" @click="hanleLogin()">登入</el-button>
+      <el-button @click="removeLogin()">清除</el-button>
     </div>
   </div>
 </template>
 
 <script>
-import { ref, reactive, computed, onMounted, onBeforeMount} from 'vue'
+import { ref, reactive, computed, onMounted, onBeforeMount } from 'vue'
+import Cookies from 'js-cookie'
+import { useRouter } from 'vue-router'
 
 export default {
   name:'Login',
   setup(){
-    
-    let loginForm =reactive({
+    const router = useRouter()
+    let loginForm = reactive({
       username:'',
       password:'',
-      token:''
+      token:'3sfjsdlkfj3254jlkdslj'
     })
+    const hanleLogin = () =>{
+      const user = loginForm.username
+      const pwd = loginForm.password
+      const token = loginForm.token
+      if( user !=='' && pwd !==''){
+        Cookies.set('login', JSON.stringify(loginForm))
+        if (Cookies.get('login') && loginForm.token){
+          router.push({ name:'Dashboard' })
+        }
+      }else{
+        alert('帳號密碼不能為空')
+      }
+    } 
+    const removeLogin = () =>{
+      loginForm.username='',
+      loginForm.password=''
+    }
+    const removeCookie = onMounted( () =>{
+      Cookies.remove('login')
+    })
+    
     return{
       loginForm,
-      
+      hanleLogin,
+      removeLogin,
+      removeCookie
     }
   }
 }
