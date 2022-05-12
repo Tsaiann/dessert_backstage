a6718a2b9032a510baf0409587d65c95c4c7b1e8
ann <ann110149@gmail.com>
2022-05-12 23:05:54 +0800

    add button

--- src/components/guideLine.vue
+++ src/components/guideLine.vue
@@ -1,10 +1,13 @@
 <template>
-  <div class="guide_line">
+  <div class="guide_line row horizontal space_between">
     <p>{{ msg }}</p>
+    <div data-space-right="1rem">
+      <el-button type="info" plain size="small">變更密碼</el-button>
+      <el-button type="info" plain size="small">登出</el-button>
+    </div>
   </div>
 </template>
 <script>
-
 export default {
   name:'GuideLine',
   props:{
@@ -12,8 +15,9 @@ export default {
   },
   setup(props){
     const msg = props.msg
+
     return{
-      msg
+      msg,
     }
   }
 }
